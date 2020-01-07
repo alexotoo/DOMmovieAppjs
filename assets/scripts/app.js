7e7c7b5d45763582400ad4b3288278453ee62dd7
Alex Otoo <alexotoous@outlook.com>
2020-01-07 01:08:26 +0000

    deletehandle complt

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,8 +6,9 @@ const getaddMovieBtn = document.querySelector(".btn--success");
 const getAddbackdrop = document.getElementById("backdrop");
 const getUserInputs = getAddMovieModal.querySelectorAll("input");
 const getEntryText = document.getElementById("entry-text");
+const getMovieEl = document.getElementById("movie-list");
 
-const movies = [];
+let movies = [];
 
 //show added movie
 const updateUI = () => {
@@ -37,9 +38,10 @@ const closeBackdropHandler = () => {
 };
 
 //add new movies to UI
-renderNewMovieEl = (title, imageUrl, rating) => {
+renderNewMovieEl = (id, title, imageUrl, rating) => {
   const newMovieEl = document.createElement("li");
   newMovieEl.className = "movie-element";
+  newMovieEl.id = id;
   newMovieEl.innerHTML = `
     <div class="movie-element__image">
     <img src= "${imageUrl}" alt="${title}">
@@ -47,10 +49,34 @@ renderNewMovieEl = (title, imageUrl, rating) => {
     <div class="movie-element__info">
     <h2>${title}</h2>
     <p>${rating}/5 stars</p>
+    <button class="deletebtn">X</button>
     </div>
     `;
   const getlistadd = document.getElementById("movie-list");
   getlistadd.append(newMovieEl);
+};
+
+//delete movie item from DOM
+deleteMovieUI = el => {
+  el = el.target.parentNode.parentNode.id;
+  const elid = document.getElementById(el);
+  // console.log(el);
+  // console.log(elid);
+  elid.parentNode.removeChild(elid);
+
+  // delete movie item from data
+  let filteredMovies = movies.filter(filtered => {
+    return filtered.id !== parseInt(el);
+  });
+
+  //update movies list
+  movies = filteredMovies;
+  console.log(movies);
+};
+
+//deletes a movie from DOM and Data
+deleteMovieHandler = el => {
+  deleteMovieUI(el);
 };
 
 //get values/movies info function
@@ -73,6 +99,7 @@ const addMovieHandler = () => {
   }
 
   let newMovies = {
+    id: Math.round(Date.now() + Math.random()),
     title: titleValue,
     image: imageUrlValue,
     rating: ratingValue
@@ -81,7 +108,12 @@ const addMovieHandler = () => {
   movies.push(newMovies);
   console.log(movies);
   closeBackdropHandler();
-  renderNewMovieEl(newMovies.title, newMovies.image, newMovies.rating);
+  renderNewMovieEl(
+    newMovies.id,
+    newMovies.title,
+    newMovies.image,
+    newMovies.rating
+  );
   clearInputFields();
   updateUI();
 };
@@ -98,3 +130,4 @@ getStartAddMovieBtn.addEventListener("click", addMovieModalHandler);
 getCloseBackdrop.addEventListener("click", closeBackdropHandler);
 getAddbackdrop.addEventListener("click", closeBackdropHandler);
 getaddMovieBtn.addEventListener("click", addMovieHandler);
+getMovieEl.addEventListener("click", deleteMovieHandler);
